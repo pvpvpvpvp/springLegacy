96d6bda892067a9d77f221bc2c3cfcbb7caab6e8
pvpvpvpvp <77872841+pvpvpvpvp@users.noreply.github.com>
2022-05-27 14:00:47 +0900

    swagger TEST

--- src/main/java/org/conan/config/ServletConfig.java
+++ src/main/java/org/conan/config/ServletConfig.java
@@ -31,6 +31,10 @@ public class ServletConfig implements WebMvcConfigurer {
 	  public void addResourceHandlers(ResourceHandlerRegistry registry) {
 	    registry.addResourceHandler("/resources/**")
 	        .addResourceLocations("/resources/");
+	    registry
+        	.addResourceHandler("/swagger-ui.html")
+        	.addResourceLocations("classpath:/META-INF/resources/");
+	    registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
 	  }
 	 
 	}
--- src/main/java/org/conan/config/SwaggerConfiguration.java
+++ src/main/java/org/conan/config/SwaggerConfiguration.java
@@ -1,19 +1,12 @@
 package org.conan.config;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpHeaders;
 
 import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Parameter;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -28,22 +21,10 @@ public class SwaggerConfiguration {
 
 	    @Bean
 	    public Docket api() {
-	        Parameter parameterBuilder = new ParameterBuilder()
-	            .name(HttpHeaders.AUTHORIZATION)
-	            .description("Access Tocken")
-	            .modelRef(new ModelRef("string"))
-	            .parameterType("header")
-	            .required(false)
-	            .build();
-
-	        List<Parameter> globalParamters = new ArrayList<>();
-	        globalParamters.add(parameterBuilder);
-
 	        return new Docket(DocumentationType.SWAGGER_2)
-	                .globalOperationParameters(globalParamters)
 	                .apiInfo(apiInfo())
 	                .select()
-	                .apis(RequestHandlerSelectors.basePackage("org.conan.*"))
+	                .apis(RequestHandlerSelectors.any())
 	                .paths(PathSelectors.any())
 	                .build();
 	    }
@@ -55,4 +36,5 @@ public class SwaggerConfiguration {
 			.description(API_DESCRIPTION)
 			.build();
 	    }
-	}
+	    
+}
